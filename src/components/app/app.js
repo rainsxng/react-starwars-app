59d6d67f20e6f5e8c304703d7beb504a828135de
Dmitry Bohdanov <starsettime@gmail.com>
2019-07-27 22:20:36 +0300

    Added item-list, person details, random planet components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,29 @@
-import React, {Component} from 'react'
-import Header from './../header'
 
-export default class App extends Component {
-    render() {
-        return (
-            <Header />
-        );
-    }
-}
+import React from 'react';
+
+import Header from '../header';
+import RandomPlanet from '../random-planet';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+
+import './app.css';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <RandomPlanet />
+
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
